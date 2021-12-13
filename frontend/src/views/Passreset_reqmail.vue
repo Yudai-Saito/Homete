57a6d7b3ea90285fd56b8c7babe66544d46e073f
RenShiratori <ren67nm8@outlook.com>
2021-12-13 02:15:30 +0000

    Add: パスワード再設定用のメールアドレス要求ページ #10
    ログインページにてパスワードの再設定を選択した際に表示する、メールアドレスを要求するページを作成した。

---
+++ frontend/src/views/Passreset_reqmail.vue
@@ -0,0 +1,40 @@
+<template>
+	<v-app>
+		<v-card width="400px" class="mx-auto my-auto pa-3">
+			<v-card-title>
+				<h1 class="headline">パスワード再設定</h1>
+				<p><font size="-1">ご利用中のメールアドレスを入力してください。パスワード再設定のためのURLをお送りします。</font></p>
+			</v-card-title>
+			<v-card-text>
+				<v-form>
+					<v-text-field
+						prepend-icon="mdi-email"
+						label="メールアドレス"
+						v-model="email"
+					/>
+					<v-card-actions>
+							<v-btn
+								class="info ml-auto mt-5"
+								@click="submit">
+							再設定メールを送信
+							</v-btn>
+					</v-card-actions>
+				</v-form>
+			</v-card-text>
+		</v-card>
+	</v-app>
+</template>
+
+
+<script>
+	export default{
+		name: 'Passreset_reqmail',
+		data(){
+			return{
+				email: '',
+			}
+		},
+		methods:{
+		},
+	}
+</script>
