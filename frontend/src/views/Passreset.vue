6b2464c65f5a0bd8c94bbd646885c47196be838f
RenShiratori <ren67nm8@outlook.com>
2021-12-13 01:57:06 +0000

    Add: パスワード再設定用ページ #10
    ログインページを参考にパスワード再設定用ページのテンプレートを作成した。

---
+++ frontend/src/views/Passreset.vue
@@ -0,0 +1,53 @@
+<template>
+	<v-app>
+		<v-card width="400px" class="mx-auto my-auto pa-3">
+			<v-card-title>
+				<h1 class="headline">パスワード再設定</h1>
+			</v-card-title>
+			<v-card-text>
+				<v-form>
+					<v-text-field
+						v-bind:type="showPassword ? 'text' : 'password'"
+						prepend-icon="mdi-lock"
+						v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+						label="パスワード"
+						@click:append="showPassword = !showPassword"
+						v-model="password"
+					/>
+					<v-text-field
+						v-bind:type="showPasswordConfirmation ? 'text' : 'password'"
+						prepend-icon="mdi-lock"
+						v-bind:append-icon="showPasswordConfirmation ? 'mdi-eye' : 'mdi-eye-off'"
+						label="パスワード確認"
+						@click:append="showPasswordConfirmation = !showPasswordConfirmation"
+						v-model="password_confirmation"
+					/>
+					<v-card-actions>
+							<v-btn
+								class="info ml-auto mt-5"
+								@click="submit">
+							パスワードを再設定する
+							</v-btn>
+					</v-card-actions>
+				</v-form>
+			</v-card-text>
+		</v-card>
+	</v-app>
+</template>
+
+
+<script>
+	export default{
+		name: 'Passreset',
+		data(){
+			return{
+				showPassword: false,
+				showPasswordConfirmation: false,
+				password: '',
+				password_confirmation: '',
+			}
+		},
+		methods:{
+		},
+	}
+</script>
