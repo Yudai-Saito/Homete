995fd1665bb36564f63d2746e7d4583609330987
YudaiSaito <yudai.saito.dev@gmail.com>
2021-12-13 21:10:05 +0900

    Change: ダブルクオーテーションに統一 #20

--- backend/route/user.py
+++ backend/route/user.py
@@ -4,14 +4,14 @@ from app.app import mail
 
 from flask_mail import Message
 
-user = Blueprint('user', __name__, url_prefix='/user')
+user = Blueprint("user", __name__, url_prefix="/user")
 
-@user.route("/signup/mail", methods=['POST'])
+@user.route("/signup/mail", methods=["POST"])
 def login():
 	user_email = request.json["user_email"]
 
 	#メールバリデーション
-	if re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', user_email):
+	if re.match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", user_email):
 
 		msg = Message("Hometeメール認証", recipients=[user_email])
 		msg.body = ""
