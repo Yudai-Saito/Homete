6aaf9a185d4074d558be3ca1023179081aaa5cfd
RenShiratori <ren67nm8@outlook.com>
2021-12-13 09:10:49 +0000

    Change: メールアドレスを要求するフォーム #10
    メールアドレスを要求するフォームを他のページでも使うため、componentsに移した。
    また、入力を促すメッセージを見やすいように改行した。

--- frontend/src/views/Passreset_reqmail.vue
+++ frontend/src/views/Passreset_reqmail.vue
@@ -3,38 +3,21 @@
 		<v-card width="400px" class="mx-auto my-auto pa-3">
 			<v-card-title>
 				<h1 class="headline">パスワード再設定</h1>
-				<p><font size="-1">ご利用中のメールアドレスを入力してください。パスワード再設定のためのURLをお送りします。</font></p>
+				<p><font size="-1">ご利用中のメールアドレスを入力してください。<br>パスワード再設定のためのURLをお送りします。</font></p>
 			</v-card-title>
-			<v-card-text>
-				<v-form>
-					<v-text-field
-						prepend-icon="mdi-email"
-						label="メールアドレス"
-						v-model="email"
-					/>
-					<v-card-actions>
-							<v-btn
-								class="info ml-auto mt-5"
-								@click="submit">
-							再設定メールを送信
-							</v-btn>
-					</v-card-actions>
-				</v-form>
-			</v-card-text>
+			<Requestmail />
 		</v-card>
 	</v-app>
 </template>
 
 
 <script>
+	import Requestmail from '../components/Requestmail'
+
 	export default{
 		name: 'Passreset_reqmail',
-		data(){
-			return{
-				email: '',
-			}
-		},
-		methods:{
+		components: {
+			Requestmail,
 		},
 	}
 </script>
