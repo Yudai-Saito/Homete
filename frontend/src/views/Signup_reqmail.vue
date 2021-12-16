433107328f08dc1f5d13454a07efcbfcc9e974a4
RenShiratori <ren67nm8@outlook.com>
2021-12-16 06:35:20 +0000

    Add: 新規登録用のメールアドレス要求ページ #27
    新規登録をする際に初めにメールアドレスを要求するページを作成した。

---
+++ frontend/src/views/Signup_reqmail.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-app>
+		<v-card width="400px" class="mx-auto my-auto pa-3">
+			<v-card-title>
+				<h1 class="headline">新規登録</h1>
+				<p><font size="-1">最初に受信可能なメールアドレスを入力してください。</font></p>
+			</v-card-title>
+			<Requestmail />
+		</v-card>
+	</v-app>
+</template>
+
+
+<script>
+	import Requestmail from '../components/Requestmail'
+
+	export default{
+		name: ' Signup_reqmail',
+		components: {
+			Requestmail,
+		},
+	}
+</script>
