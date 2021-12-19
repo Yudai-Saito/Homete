91c7c93702e348a321e030dae1c04a3aa8b66131
YudaiSaito <yudai.saito.dev@gmail.com>
2021-12-19 13:06:19 +0900

    Change: tokenをメソッド内で生成 #37
    関数を二重に踏んでいたので直接生成するように変更

--- backend/route/user.py
+++ backend/route/user.py
@@ -1,6 +1,7 @@
 from re import match
 from os import environ
 from hashlib import sha256
+from datetime import timedelta
 
 from flask import Blueprint, request, jsonify
 from flask_mail import Message
@@ -8,7 +9,6 @@ from flask_jwt_extended import jwt_required, create_access_token, create_refresh
 from sqlalchemy import or_
 
 from app.app import mail, db
-from route.token import create_mail_token
 from models.models import User
 
 user = Blueprint("user", __name__, url_prefix="/user")
@@ -25,9 +25,12 @@ def signup_mail():
 	#メールバリデーション
 	if match(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", user_email):
 
+		#TOKEN生成
+		access_token = create_access_token(expires_delta=timedelta(seconds=300), identity=user_email)
+
 		msg = Message("Hometeメール認証", recipients=[user_email])
 		msg.body = "Hometeへようこそ!\n5分以内にこのURLから登録を済ませてください!\n" \
-										+ SIGNUP_URL + "?jwt=" + create_mail_token(user_email)
+										+ SIGNUP_URL + "?jwt=" + access_token
 		mail.send(msg)
 
 		return jsonify({"status": "success"}), 200
