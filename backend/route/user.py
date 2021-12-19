27ce2e3fc2b039a8a0bed6b5ec4687361074d998
YudaiSaito <yudai.saito.dev@gmail.com>
2021-12-19 13:00:29 +0900

    Add: ユーザログインAPI #37
    新規登録メール送信の関数名がloginになり競合していたので修正。

--- backend/route/user.py
+++ backend/route/user.py
@@ -4,7 +4,8 @@ from hashlib import sha256
 
 from flask import Blueprint, request, jsonify
 from flask_mail import Message
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token
+from sqlalchemy import or_
 
 from app.app import mail, db
 from route.token import create_mail_token
@@ -15,7 +16,7 @@ user = Blueprint("user", __name__, url_prefix="/user")
 SIGNUP_URL = "{}/user/signup".format(environ["FRONTEND_URL"])
 
 @user.route("/signup/mail", methods=["POST"])
-def login():
+def signup_mail():
 	"""新規登録用のメールを送信
 	新規登録時にメールアドレスを受け取り、認証URLを送信する
 	"""
@@ -53,3 +54,30 @@ def verify():
 	db.session.commit()
 
 	return jsonify({"status": "success"}), 200
+
+@user.route("/login", methods=["POST"])
+def login():
+	"""ユーザログイン
+	ユーザIDとパスワードを受け取り、認証を行う
+	"""
+	try:
+		#JSONからユーザー情報取り出し
+		user_info = request.json["user_info"]
+		hashed_password = request.json["hashed_password"]
+
+		#DBからユーザ登録情報取得
+		user = User.query.filter(or_(User.user_id==user_info, User.user_email==user_info)).first()
+
+		#パスワードのハッシュ化
+		hashed_password = sha256((hashed_password + user.user_id + environ["SALT"]).encode("utf-8")).hexdigest()
+
+		#パスワードが一致しているか確認
+		if user.hashed_password == hashed_password:
+			#Token生成
+			access_token = create_access_token(identity=user.user_id)
+			refresh_token = create_refresh_token(identity=user.user_id)
+			return jsonify({"status": "success", "access_token": access_token, "refresh_token": refresh_token}), 200
+		else:
+			raise Exception("password error")	
+	except:
+		return jsonify({"status": "error"}), 400
