2cdba2e8214594cc0bb280bf642d023214c060b1
YudaiSaito <yudai.saito.dev@gmail.com>
2021-12-23 00:36:25 +0900

    Add: リアクション削除API #48

--- backend/route/post.py
+++ backend/route/post.py
@@ -49,3 +49,33 @@ def reaction_count_up():
 		return jsonify({"status": "success"}), 200
 	except:
 		return jsonify({"status": "error"}), 400
+
+@post.route("/reaction", methods=["DELETE"])
+@auth_required
+def reaction_count_down():
+	"""投稿のリアクションをデクリメントする
+	jsonからpost_id, reactionを取得する
+	リアクションがある場合は、デクリメントし、ない場合は400を返す
+	"""
+	try:
+		post_id = request.json["post_id"]
+		reaction = request.json["reaction"]
+
+		#投稿に既にリアクションがあるか確認 
+		if db.session.query(Post_reaction.query.filter(Post_reaction.post_id == post_id, Post_reaction.reaction == reaction).exists()).scalar() == True:
+			#減少するリアクションを習得して、デクリメントする
+			post = db.session.query(Post_reaction).filter(Post_reaction.post_id == post_id, Post_reaction.reaction == reaction).first()
+			post.reaction_count -= 1
+
+			#リアクションが0以下になった場合、レコードを削除
+			if post.reaction_count <= 0:
+				db.session.delete(post)
+
+			db.session.commit()
+		else:
+			#リアクションが無い場合は400を返す
+			raise("not exist reaction")
+
+		return jsonify({"status": "success"}), 200
+	except:
+		return jsonify({"status": "error"}), 400
