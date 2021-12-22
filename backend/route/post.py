3928907de7fcad6f06fac73946f23f1d678dcc58
YudaiSaito <yudai.saito.dev@gmail.com>
2021-12-22 22:04:45 +0900

    Change: 認証方式をJWTからRedisへ #66

--- backend/route/post.py
+++ backend/route/post.py
@@ -1,20 +1,21 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from app.app import db
+from app.app import db, redis
 from models.models import Homete_post
+from route.token import auth_required
 
 post = Blueprint("post", __name__, url_prefix="/post")
 
+
 @post.route("", methods=["POST"])
-@jwt_required()
+@auth_required
 def post_receive():
 	"""投稿を受け取り、DBに保存する
 	jwtからuser_idを取得する
 	jsonから投稿内容を取得する
 	"""
 	try:	
-		user_id = get_jwt_identity()
+		user_id = redis.get(request.cookies.get("token"))
 		post_content = request.json["post_content"]
 
 		db.session.add(Homete_post(user_id = user_id, post_content = post_content))
