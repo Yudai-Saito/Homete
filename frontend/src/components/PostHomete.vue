0c7ede94e35fad999d8e454975586336c219b807
RenShiratori <ren67nm8@outlook.com>
2021-12-22 15:09:17 +0000

    Add: TOP画面で使う投稿フォームのテンプレート #44
    現在clear-iconが改行に合わせてずれる現象が発生中

---
+++ frontend/src/components/PostHomete.vue
@@ -0,0 +1,64 @@
+<template>
+	<v-container fluid>
+		<v-card
+			:loading="loading"
+			:disabled="form || loading"
+			outlined
+		>
+			<template slot="progress">
+				<v-progress-linear
+					color="deep-purple"
+					height="10"
+					indeterminate
+				></v-progress-linear>
+			</template>
+			<v-form
+				ref="form"
+			>
+				<v-textarea
+					clearable
+					solo
+					flat
+					auto-grow
+					rows="3"
+					label="なにを褒めてもらう？"
+					v-model="homete"
+				></v-textarea>
+				<v-divider class="mx-4"></v-divider>
+				<v-card-actions>
+						<v-btn
+							class="info ml-auto"
+							@click="submit">
+								投稿する
+						</v-btn>
+				</v-card-actions>
+			</v-form>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+export default{
+	name: 'PostHomete',
+	data(){
+		return{
+			form: false,
+			loading: false,
+			homete: '',
+		}
+	},
+	methods: {
+		submit: function() {
+			this.loading = true
+			setTimeout(() => {
+				this.formReset()
+				this.loading = false
+			}, 1500)
+		},
+		formReset () {
+			this.$refs.form.reset()
+			this.params = { homete: ''}
+		},
+	},
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
 	<v-app>
 		<v-navigation-drawer app>Navigation Lists</v-navigation-drawer>
-		<v-app-bar color="primary" dark app>
-			<v-toolbar-title>Vuetify</v-toolbar-title>
-		</v-app-bar>
-		<v-footer color="primary" dark app>
-			Vuetify
-		</v-footer>
+		<PostHomete />
 	</v-app>
 </template>
 
 <script>
-
+import PostHomete from '../components/PostHomete'
 export default {
 	name: "Home",
+	components: {
+		PostHomete,
+	},
 };
 </script>
