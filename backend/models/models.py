58ea4ff6899be9ca4a610f5d65a1b9a55931db72
YudaiSaito <yudai.saito.dev@gmail.com>
2021-12-22 22:29:29 +0900

    Change: モデルをMySQL仕様へ #70

--- backend/models/models.py
+++ backend/models/models.py
@@ -6,8 +6,8 @@ class User(db.Model):
 
     user_id = db.Column(db.String(15), primary_key=True)
     user_name = db.Column(db.String(15))
-    user_email = db.Column(db.String, unique=True)
-    hashed_password = db.Column(db.String)
+    user_email = db.Column(db.String(256), unique=True)
+    hashed_password = db.Column(db.String(64))
     created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     updated_at = db.Column(db.DateTime)
     deleted_at = db.Column(db.DateTime)
@@ -27,6 +27,6 @@ class Post_reaction(db.Model):
     __tablename__ = "post_reactions"
 
     reaction_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    post_id = db.Column(db.String)
-    reaction = db.Column(db.String)
+    post_id = db.Column(db.Integer)
+    reaction = db.Column(db.String(10))
     reaction_count = db.Column(db.Integer)
