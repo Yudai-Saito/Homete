0c84eb7a09ec1399d3d397b3e56a3edfe782b3ac
RenShiratori <ren67nm8@outlook.com>
2021-12-22 21:17:10 +0900

    Remove: 使わないコンポーネントとその適応箇所 #44
    HelloWorld.vueの削除とHome.vue内の整理

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <hello-world />
+	<v-app>
+		<v-navigation-drawer app>Navigation Lists</v-navigation-drawer>
+		<v-app-bar color="primary" dark app>
+			<v-toolbar-title>Vuetify</v-toolbar-title>
+		</v-app-bar>
+		<v-footer color="primary" dark app>
+			Vuetify
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+	name: "Home",
+};
 </script>
