b0d6f74d65ea89892f4ecdb3453375254232d79f
RenShiratori <ren67nm8@outlook.com>
2021-12-26 07:02:15 +0000

    Add: TOP画面に表示する投稿のテンプレート #45

---
+++ frontend/src/components/DisplayHomete.vue
@@ -0,0 +1,45 @@
+<template>
+	<v-container fluid>
+		<v-card
+			outlined
+		>
+			<h4 class="ml-4 mt-2">匿名さん</h4>
+			<v-divider class="mx-4"></v-divider>
+			<v-card-text>
+				{{homete}}
+			</v-card-text>
+			<v-card-actions>
+					<ReactionButton
+						v-for="reaction in reactions"
+						:key="reaction"
+						:reactionIcon=reaction
+					/>
+			</v-card-actions>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+import ReactionButton from '../components/ReactionButton'
+export default{
+	name: 'DisplayHomete',
+	data(){
+		return{
+			homete: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore',
+			reactions:{
+				good: '👍',
+				interesting: '👀',
+				great: '💯',
+				like: '🥰',
+				congratulations: '🎉',
+			},
+		}
+	},
+	components:{
+		ReactionButton,
+	},
+	methods: {
+		
+	},
+}
+</script>
---
+++ frontend/src/components/ReactionButton.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-btn
+		class="justify-start"
+		@click="countup"
+		elevation='0'
+		small
+	>
+		<h2>{{reactionIcon}}</h2>{{reactionCount}}
+	</v-btn>
+</template>
+
+<script>
+export default{
+	name: 'ReactionButton',
+	data(){
+		return{
+			reactionCount: 0,
+		}
+	},
+	props:[
+		'reactionIcon'
+	],
+	methods: {
+		countup: function(){
+			this.reactionCount += 1
+		}
+	},
+}
+</script>
