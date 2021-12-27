1ef4f99bbe846edc1f358da9dfb4be182f1e05e0
RenShiratori <ren67nm8@outlook.com>
2021-12-27 08:41:27 +0000

    Add: トップ画面用のファイルのリネームとテンプレートの作成 #43

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Top from '../views/Top.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
 	{
 		path: '/',
-		name: 'Home',
-		component: Home
+		name: 'Top',
+		component: Top
 	},
 	{
 		path: '/About',
---
+++ frontend/src/views/Top.vue
@@ -0,0 +1,75 @@
+<template>
+	<v-app>
+		<v-container fluid class="mainContainer mx-auto">
+			<v-row>
+				<v-col cols="2" class="d-none d-sm-block ma-0 pa-0 leftMenu">
+					<SideMenu />
+				</v-col>
+
+				<v-divider vertical></v-divider>
+
+				<v-col cols="12" sm="8" md="7" class="">
+					<PostHomete v-on:cardVisible='noticeVisible' v-if="cardVisible"/>
+					<DisplayHomete />
+				</v-col>
+
+				<v-divider vertical></v-divider>
+
+				<v-col md="3" class="hidden-sm-and-down ma-0 pa-0 rightMenu">
+					<h2 align="center">column</h2>
+				</v-col>
+			</v-row>
+		</v-container>
+	</v-app>
+</template>
+<style>
+	.testColorA{
+		background-color: cyan;
+	}
+	.testColorB{
+		background-color: burlywood;
+	}
+	.mainContainer{
+		max-width: 1200px;
+		width: 100%;
+		height: 100%;
+	}
+	.leftMenu{
+		width: 115pt;
+		min-width: 115pt;
+		max-width: 115pt;
+		flex: none;
+	}
+	.rightMenu{
+		min-width: 115pt;
+	}
+</style>
+
+
+<script>
+import PostHomete from '../components/PostHomete'
+import DisplayHomete from '../components/DisplayHomete'
+import SideMenu  from '../components/SideMenu'
+export default {
+	name: "Top",
+	data(){
+		return{
+			cardVisible: true,
+		}
+	},
+	components: {
+		PostHomete,
+		DisplayHomete,
+		SideMenu,
+	},
+	methods: {
+		noticeVisible: function(childCardVisible){
+			this.cardVisible = childCardVisible
+			console.log(this.cardVisible)
+		}
+	},
+	mounted(){
+		console.log(this.$vuetify.breakpoint)
+	}	
+};
+</script>
