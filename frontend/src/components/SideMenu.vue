ed2ac31552cff2f5668ad8d4ed7b9b4036abe9b9
RenShiratori <ren67nm8@outlook.com>
2021-12-27 08:42:45 +0000

    Add: TOP画面に表示するサイドメニュー用のConponents #43

---
+++ frontend/src/components/SideMenu.vue
@@ -0,0 +1,61 @@
+<template>
+	<v-container class="sideContainer">
+		<v-row justify="start" class="mt-7 sideMenuButton">
+			<v-btn
+				small
+				rounded
+				text
+			>
+				<v-icon>
+					mdi-pen-plus
+				</v-icon>
+				投稿する
+			</v-btn>
+		</v-row>
+		<v-row justify="start" class="sideMenuButton">
+			<v-btn
+				small
+				rounded
+				text
+			>
+				<v-icon>
+					mdi-account-cog
+				</v-icon>
+				アカウント管理
+			</v-btn>
+		</v-row>
+		<v-row justify="start" class="sideMenuButton">
+			<v-btn
+				small
+				rounded
+				text
+			>
+				<v-icon>
+					mdi-logout
+				</v-icon>
+				ログアウト
+			</v-btn>
+		</v-row>
+	</v-container>
+</template>
+<style>
+	.sideContainer{
+		height: 100vh;
+	}
+	.sideMenuButton{
+		margin-bottom: 5pt;
+	}
+</style>
+
+<script>
+export default{
+	name: 'SideMenu',
+	data(){
+		return{
+		}
+	},
+	methods: {
+		
+	},
+}
+</script>
