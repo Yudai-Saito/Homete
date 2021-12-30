23fd3ebbb8f534e409b360a8d3645cbf4e1b186d
RenShiratori <ren67nm8@outlook.com>
2021-12-30 08:06:21 +0000

    Add: 渡された値を元に現在のボタンの状態を反映して表示できるようにした #87

--- frontend/src/components/ReactionButton.vue
+++ frontend/src/components/ReactionButton.vue
@@ -1,13 +1,31 @@
 <template>
 	<v-btn
 		class="justify-start"
-		@click="countup"
+		@click="count"
 		elevation='0'
 		small
+		v-if="!reactionFlag"
+	>
+		<h2>{{reactionIcon}}</h2>{{reactionCount}}
+	</v-btn>
+	<v-btn
+		class="justify-start pushedButton"
+		@click="count"
+		elevation='0'
+		small
+		outlined
+		color="#3949AB"
+		v-else
 	>
 		<h2>{{reactionIcon}}</h2>{{reactionCount}}
 	</v-btn>
 </template>
+
+<style>
+	.pushedButton{
+		background-color: "#3949AB,0.5";
+	}
+</style>
 
 <script>
 export default{
@@ -15,15 +33,36 @@ export default{
 	data(){
 		return{
 			reactionCount: 0,
+			reactionFlag: false,
 		}
 	},
 	props:[
-		'reactionIcon'
+		'reactionIcon',
+		'postReaction',
+		'userReaction',
 	],
 	methods: {
-		countup: function(){
-			this.reactionCount += 1
+		count: function(){
+			if(this.reactionFlag){
+				this.reactionCount -= 1
+				this.reactionFlag = false
+			}
+			else{
+				this.reactionCount += 1
+				this.reactionFlag = true
+			}
 		}
+	},
+	created() {
+		this.postReaction.forEach(item => {
+			console.log(item)
+			if(item.reaction == this.reactionIcon){
+				this.reactionCount = item.count - 0
+			}
+			if(this.userReaction.includes(this.reactionIcon)){
+				this.reactionFlag = true
+			}
+		})
 	},
 }
 </script>
