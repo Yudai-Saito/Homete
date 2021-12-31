0de58af8eb5033fe8b0529dc648df656c933473a
RenShiratori <ren67nm8@outlook.com>
2021-12-31 08:21:19 +0000

    Fix: スマホ画面時に崩壊していたデザインを修正 #90

--- frontend/src/components/ReactionButton.vue
+++ frontend/src/components/ReactionButton.vue
@@ -1,13 +1,39 @@
 <template>
-	<v-btn
-		class="justify-start"
-		@click="countup"
-		elevation='0'
-		small
-	>
-		<h2>{{reactionIcon}}</h2>{{reactionCount}}
-	</v-btn>
+	<div class="btnDiv">
+		<v-btn
+			class="d-none d-sm-block reactBtnA"
+			@click="countup"
+			elevation='0'
+			small
+		>
+			<h2>{{reactionIcon}}</h2>{{reactionCount}}
+		</v-btn>
+
+		<v-btn
+			class="d-block d-sm-none reactBtnB"
+			@click="countup"
+			elevation='0'
+			x-small
+		>
+			<h3>{{reactionIcon}}</h3>{{reactionCount}}
+		</v-btn>
+	</div>
 </template>
+<style>
+	.btnDiv{
+		margin: 0;
+		padding: 0;
+	}
+	.reactBtnA{
+		margin: 0;
+		margin-right: 9px;
+	}
+	.reactBtnB{
+		margin: 0;
+		min-height: 22px;
+		margin-right: 3px;
+	}
+</style>
 
 <script>
 export default{
