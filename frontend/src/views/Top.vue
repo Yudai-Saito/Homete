34fbf9e2895094303bd32b623da8a148897c0c60
RenShiratori <ren67nm8@outlook.com>
2021-12-31 05:01:30 +0000

    Add: スマホサイズ時のハンバーガーメニューと投稿ボタンの追加 #90

--- frontend/src/views/Top.vue
+++ frontend/src/views/Top.vue
@@ -13,14 +13,33 @@
 					<SideMenu v-on:overlay='overlayCard' />
 				</v-col>
 
-				<v-divider vertical></v-divider>
+				<v-app-bar
+					elevation=0 color=transparent dense class="d-block d-sm-none topMenu"
+				>
+					<v-app-bar-nav-icon @click="drawer = true" x-large class="d-block d-sm-none navButton"></v-app-bar-nav-icon>
+				</v-app-bar>
+				<v-navigation-drawer
+					v-model="drawer"
+					fixed
+					temporary
+					v-bind:width="200"
+				>
+					<SideMenu v-on:overlay='overlayCard' />
+				</v-navigation-drawer>
+				<v-btn elevation=3 fab x-large color="white" icon class="d-block d-sm-none postButton" @click="overlay = true">
+					<v-icon>
+						mdi-pen-plus
+					</v-icon>
+				</v-btn>
+
+				<v-divider vertical class="d-none d-sm-block"></v-divider>
 
 				<v-col cols="12" sm="8" md="8" class="subContainer">
 					
 					<DisplayHomete />
 				</v-col>
 
-				<v-divider vertical></v-divider>
+				<v-divider vertical class="d-none d-sm-block"></v-divider>
 
 				<v-col md="2" class="hidden-sm-and-down ma-0 pa-0 rightMenu">
 					<h2 align="center">column</h2>
@@ -53,6 +72,25 @@
 	.rightMenu{
 		min-width: 115pt;
 	}
+	.topMenu{
+		width: 100vw;
+		min-width: 100vw;
+		max-width: 100vw;
+		flex: none;
+		margin: 0;
+		padding: 0;
+	}
+	.postButton{
+		position: fixed;
+		left: 30px;
+		bottom: 30px;
+		background-color: #1DA1F2;
+	}
+	.navButton{
+		position: fixed;
+		left: 30px;
+		top: 10px;
+	}
 </style>
 
 
@@ -65,6 +103,7 @@ export default {
 	data(){
 		return{
 			overlay: false,
+			drawer: false,
 		}
 	},
 	components: {
@@ -79,6 +118,7 @@ export default {
 		},
 		overlayCard: function(childOverlay){
 			this.overlay = childOverlay
+			this.drawer = false
 		}
 	},
 	mounted(){
