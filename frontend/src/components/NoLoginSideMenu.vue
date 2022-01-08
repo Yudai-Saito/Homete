bc4e9e84761585896faeb1a96615acce8addb837
RenShiratori <ren67nm8@outlook.com>
2022-01-08 08:59:27 +0000

    Add: メニューバーにAboutと利用規約のボタン #103

--- frontend/src/components/NoLoginSideMenu.vue
+++ frontend/src/components/NoLoginSideMenu.vue
@@ -26,6 +26,35 @@
 				ログイン
 			</v-btn>
 		</v-row>
+		<v-row
+			justify="end"
+			align-content="end"
+			class="sideMenuUnderButton"
+		>
+			<v-btn
+				small
+				rounded
+				text
+				href="About"
+				class="underButton"
+			>
+				<v-icon>
+					mdi-information-outline
+				</v-icon>
+				About
+			</v-btn>
+			<v-btn
+				small
+				rounded
+				text
+				class="underButton mb-7"
+			>
+				<v-icon>
+					mdi-clipboard-text-outline
+				</v-icon>
+				利用規約
+			</v-btn>
+		</v-row>
 	</v-container>
 </template>
 <style>
@@ -33,6 +62,18 @@
 		height: 100vh;
 	}
 	.sideMenuButton{
+		margin-bottom: 5pt;
+	}
+	.sideMenuUnderButton{
+		position: relative;
+		bottom: 145px;
+		width: 100%;
+		height: 100%;
+		margin-left: auto;
+	}
+	.underButton{
+		margin: 0;
+		padding: 0;
 		margin-bottom: 5pt;
 	}
 </style>
