f3f481a8b0b9b14e172c5bc856b7bf1f439485d4
YudaiSaito <yudai.saito.dev@gmail.com>
2022-05-10 22:37:41 +0900

    Add: ロード時のタイトル対策 #131
    ロード時のタイトルがプロジェクトフォルダ名で出てしまうため対策

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: 'HOMETE',
+        }
+    },
     css: {
         loaderOptions: { 
             sass: {
