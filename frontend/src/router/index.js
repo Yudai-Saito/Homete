ca3ca523d2685b100234e380865b1d27fabb29ff
YudaiSaito <yudai.saito.dev@gmail.com>
2022-10-15 17:08:49 +0900

    refactor: firebase移行に伴った不必要なルーティング

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -21,36 +21,6 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
     meta: { title: "HOMETEとは？" },
   },
-  {
-    path: "/login",
-    name: "Login",
-    component: () => import("../views/Login.vue"),
-    meta: { title: "ログイン" },
-  },
-  {
-    path: "/signup",
-    name: "Signup",
-    component: () => import("../views/Signup.vue"),
-    meta: { title: "新規登録" },
-  },
-  {
-    path: "/signup/mail",
-    name: "SignupReqmail",
-    component: () => import("../views/SignupReqmail.vue"),
-    meta: { title: "新規登録" },
-  },
-  {
-    path: "/passreset",
-    name: "Passreset",
-    component: () => import("../views/Passreset.vue"),
-    meta: { title: "パスワード再設定" },
-  },
-  {
-    path: "/passreset/mail",
-    name: "PassresetReqmail",
-    component: () => import("../views/PassresetReqmail.vue"),
-    meta: { title: "パスワード再設定用" },
-  },
 ];
 
 const router = new VueRouter({
