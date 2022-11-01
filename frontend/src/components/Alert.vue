7d43c33a8f9bfcac61f051a683125112cfa04782
RenShiratori <ren67nm8@outlook.com>
2022-11-01 14:52:36 +0900

    add:アラートを全てAlert.vue内で管理できるように定義 #162

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-expand-transition>
+    <v-alert
+      v-show="isAlertPost"
+      :color=alertColor[alertState]
+      text
+      type="success"
+      class="alertSucess"
+    >
+      {{ alertText[alertState] }}
+    </v-alert>
+  </v-expand-transition>
+</template>
+
+<style>
+.alertSucess {
+  width: 90%;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      //[投稿、ログイン、ログアウト]
+      alertState: 0,
+      alertColor: ["primary", "green lighten-2", "red accent-2"],
+      alertText: [
+        "投稿しました!",
+        "おかえりなさい",
+        "ログアウトが完了しました",
+      ],
+    };
+  },
+  computed: {
+    isAlertPost() {
+      return this.$store.getters.isAlertPost;
+    },
+  },
+};
+</script>
