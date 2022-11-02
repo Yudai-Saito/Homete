3814e3014e355c1c76f11054878d030e559e57c6
RenShiratori <ren67nm8@outlook.com>
2022-11-02 17:15:40 +0900

    delete:使用していないコンポーネントとvuexの削除 #162

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
-import counter from './modules/counter';
+
 import reactions from './modules/reactions';
 import isLogin from './modules/isLogin';
 import isVisiblePostHomete from './modules/isVisiblePostHomete';
@@ -14,7 +14,6 @@ export default new Vuex.Store({
   plugins: [createPersistedState()],
   strict: process.env.NODE_ENV !== 'production',
   modules: {
-    counter,
     reactions,
     isLogin,
     isVisiblePostHomete,
