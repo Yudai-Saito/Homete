4283cb40f29fd824e1a8c92056ab507872a34fe7
RenShiratori <ren67nm8@outlook.com>
2022-11-02 16:48:46 +0900

    delete:左側サイドメニューに必要のないボタンとテキストを削除 #162
    今後のUIの変更のために投稿ボタン等を削除した

--- frontend/src/components/SideMenu.vue
+++ frontend/src/components/SideMenu.vue
@@ -1,44 +1,16 @@
 <template>
   <v-container class="sideContainer">
-    <LeftMenuButton
-      usage="alertPost"
-      btnText="タイムライン"
-      btnIcon="mdi-home"
-    />
-    <LeftMenuButton
-      usage="alertLogin"
-      btnText="ヒストリー"
-      btnIcon="mdi-history"
-    />
-
     <v-row justify="start" class="mt-7 sideMenuButton">
-      <v-btn small rounded text v-on:click="visibleCard">
-        <v-icon> mdi-pen-plus </v-icon>
-        投稿する
-      </v-btn>
-    </v-row>
-    <v-row justify="start" class="sideMenuButton">
-      <v-btn small rounded text>
-        <v-icon> mdi-account-cog </v-icon>
-        アカウント管理
-      </v-btn>
-    </v-row>
-    <v-row justify="start" class="sideMenuButton">
-      <v-btn small rounded text @click="logout">
-        <v-icon> mdi-logout </v-icon>
-        ログアウト
-      </v-btn>
-    </v-row>
-    <v-row justify="end" align-content="end" class="sideMenuUnderButton">
-      <v-btn small rounded text to="/about" class="underButton">
-        <v-icon> mdi-information-outline </v-icon>
-        About
-      </v-btn>
-      <v-btn small rounded text class="underButton mb-7">
-        <v-icon> mdi-clipboard-text-outline </v-icon>
-        利用規約
-      </v-btn>
-      <p align="end" class="versionText">HOMETE v1.0.0</p>
+      <LeftMenuButton
+        usage="alertPost"
+        btnText="タイムライン"
+        btnIcon="mdi-home"
+      />
+      <LeftMenuButton
+        usage="alertLogin"
+        btnText="ヒストリー"
+        btnIcon="mdi-history"
+      />
     </v-row>
   </v-container>
 </template>
@@ -50,24 +22,6 @@
   position: relative;
   z-index: 100;
   margin-bottom: 5pt;
-}
-.sideMenuUnderButton {
-  position: relative;
-  z-index: 10;
-  width: 100%;
-  margin-left: auto;
-}
-.underButton {
-  margin: 0;
-  padding: 0;
-  margin-bottom: 5pt;
-}
-.versionText {
-  font-size: smaller;
-  position: relative;
-  top: auto;
-  bottom: 0;
-  right: 1rem;
 }
 </style>
 
