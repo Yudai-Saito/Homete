f20f64a86c8bd25c3932ba0696c8cddd7874ea22
RenShiratori <ren67nm8@outlook.com>
2022-11-08 15:47:11 +0900

    add:タイトルの追加 #166

--- frontend/src/components/util/Header.vue
+++ frontend/src/components/util/Header.vue
@@ -1,7 +1,14 @@
 <template>
   <v-app-bar absolute dense elevation="0" color="#BABDBE">
+    <v-toolbar-title class="text-h5 font-weight-bold title">
+      homete…
+    </v-toolbar-title>
   </v-app-bar>
 </template>
 
 <style>
+.title {
+  margin-left: 5%;
+  color: #494854;
+}
 </style>
