02adb0dd538a47bb9a68d860a97cfc102a892478
RenShiratori <ren67nm8@outlook.com>
2022-11-08 15:48:44 +0900

    add:ログイン/ログアウト用のボタン追加 #166

--- frontend/src/components/util/Header.vue
+++ frontend/src/components/util/Header.vue
@@ -6,6 +6,9 @@
     <v-btn class="accountBtn" color="#CFD8DC" rounded>
       <v-icon color="#494854">mdi-account-cog</v-icon>
     </v-btn>
+    <v-btn class="loginBtn" color="#CFD8DC" rounded>
+      <v-icon color="#494854">mdi-login-variant</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 
@@ -16,5 +19,9 @@
 }
 .accountBtn {
   margin-left: auto;
+}
+.loginBtn {
+  margin-left: 16px;
+  margin-right: 5%;
 }
 </style>
