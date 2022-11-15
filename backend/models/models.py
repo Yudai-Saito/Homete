8f98c1c1b96399459780065f2c6535368e7215e1
Yudai-Saito <yudai.saito.dev@gmail.com>
2022-11-15 05:00:42 +0000

    fix: 新DBにしてから旧DB情報が不要なため削除し新バージョンを生成

--- backend/models/models.py
+++ backend/models/models.py
@@ -4,8 +4,7 @@ from app import db
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(256), unique=True)
+    email = db.Column(db.String(256), primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
     deleted_at = db.Column(db.DateTime)
 
@@ -13,11 +12,17 @@ class Posts(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer)
-    face_id = db.Column(db.Integer)
-    name = db.Column(db.String(15))
-    contents = db.Column(db.String(400))
+    user_email = db.Column(db.String(256))
     private = db.Column(db.Boolean, default=True)
+    contents = db.Column(db.String(400))
+    name = db.Column(db.String(15))
+    head = db.Column(db.String(15))
+    face = db.Column(db.String(15))
+    facialhair = db.Column(db.String(15))
+    accessories = db.Column(db.String(15))
+    skincolor = db.Column(db.String(15))
+    clothingcolor = db.Column(db.String(15))
+    haircolor = db.Column(db.String(15))
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
     deleted_at = db.Column(db.DateTime)
 
@@ -33,18 +38,6 @@ class UserReactions(db.Model):
     __tablename__ = "user_reactions"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer)
+    user_email = db.Column(db.String(256))
     reaction_id = db.Column(db.Integer)
     post_id = db.Column(db.Integer)
-
-class PostFaces(db.Model):
-    __tablename__ = "post_faces"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    head = db.Column(db.String(15))
-    face = db.Column(db.String(15))
-    facialHair = db.Column(db.String(15))
-    accessories = db.Column(db.String(15))
-    skinColor = db.Column(db.String(15))
-    clothingColor = db.Column(db.String(15))
-    hairColor = db.Column(db.String(15))
