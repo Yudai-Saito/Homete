7505a6dd7db774367bb971478c5f3a581e268382
RenShiratori <ren67nm8@outlook.com>
2022-11-16 10:44:17 +0900

    add:投稿フォームの配置場所の決定

--- frontend/src/components/util/PostForm.vue
+++ frontend/src/components/util/PostForm.vue
@@ -1,63 +1,70 @@
 <template>
-  <v-container fluid>
-    <v-card
-      :loading="loading"
-      :disabled="form || loading"
-      outlined
-      class="formTxtCard"
-    >
-      <v-btn icon plain @click="closeCard" class="my-1 ml-1">
-        <v-icon color="black"> mdi-close </v-icon>
+  <v-card
+    outlined
+    class="formTxtCard rounded-xl"
+  >
+    <v-btn v-show="!logged" icon plain @click="closeCard" class="my-1 ml-1">
+      <v-icon color="black"> mdi-close </v-icon>
+    </v-btn>
+
+    <template slot="progress">
+      <v-progress-linear
+        color="#4169e1"
+        height="5"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-form class="formArea" v-model="isValid" ref="forms">
+      <v-btn
+        icon
+        plain
+        @click="clearText"
+        v-show="detectInput"
+      >
+        <v-icon small color="glay"> mdi-close-circle </v-icon>
       </v-btn>
-      <template slot="progress">
-        <v-progress-linear
-          color="#4169e1"
-          height="5"
-          indeterminate
-        ></v-progress-linear>
-      </template>
-      <v-form v-model="isValid" ref="form">
-        <v-btn
-          icon
-          plain
-          @click="clearText"
-          class="float-right mt-2 mr-2"
-          v-show="detectInput"
-        >
-          <v-icon small color="glay"> mdi-close-circle </v-icon>
-        </v-btn>
-        <v-textarea
-          label="なにを褒めてもらう？"
-          solo
-          flat
-          auto-grow
-          rows="3"
-          v-model="formTxt"
-          @input="inputText"
-          counter="100"
-          :rules="forms.inputRules"
-        ></v-textarea>
-        <v-divider class="mx-4"></v-divider>
-        <v-card-actions>
-          <v-btn
-            :disabled="!isValid || loading"
-            class="info ml-auto"
-            @click="submit"
-            elevation="0"
-            rounded
-          >
-            投稿する
-          </v-btn>
-        </v-card-actions>
-      </v-form>
-    </v-card>
-  </v-container>
+      <v-textarea
+        label="なにを褒めてもらう？"
+        solo
+        flat
+        hide-details="auto"
+        v-model="formTxt"
+        @input="inputText"
+        counter="400"
+        :rules="forms.inputRules"
+      ></v-textarea>
+      <v-divider class="mx-4"></v-divider>
+      <v-btn
+        :disabled="!isValid || loading"
+        class="info"
+        @click="submit"
+        elevation="0"
+        rounded
+      >
+        投稿する
+      </v-btn>
+    </v-form>
+  </v-card>
 </template>
 <style>
 .formTxtCard {
-  position: sticky;
-  width: 500px;
-  bottom: 200px;
+  display: flex;
+  width: 100%;
+  margin: auto;
+  padding: 0;
+  overflow: hidden;
+}
+.formArea{
+  margin: auto 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+.v-textarea textarea {
+  margin: 0 !important;
+  padding: 0 !important;
+  height: 40vh !important;
 }
 </style>
 
@@ -128,6 +135,9 @@ export default {
     displayPostForm() {
       return this.$store.getters.displayPostForm;
     },
+    logged() {
+      return this.$store.getters.logged;
+    }
   },
 };
 </script>
