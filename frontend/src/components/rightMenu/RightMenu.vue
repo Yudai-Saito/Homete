fe8ca59616c163307350a15c7cc9a37640335145
RenShiratori <ren67nm8@outlook.com>
2022-11-16 10:43:44 +0900

    add:右メニュー自体の配置調整と投稿フォームへの置き換え

--- frontend/src/components/rightMenu/RightMenu.vue
+++ frontend/src/components/rightMenu/RightMenu.vue
@@ -1,54 +1,22 @@
 <template>
-  <v-col class="rightMenu">
-    <div class="postBtn" @click="clickPost">
-      <button class="postBtnTxt"><v-icon>mdi-pen-plus</v-icon>投稿する</button>
-    </div>
-    <div class="ruleBtn">
-      <button class="ruleBtnTxt">利用規約</button>
-    </div>
+  <v-col class="rightMenu" cols="3">
+    <PostForm />
   </v-col>
 </template>
 <style>
 .rightMenu {
   display: flex;
-  flex-flow: column;
   height: 100vh;
   min-height: 100vh;
   max-height: 100vh;
   margin: 0;
   padding: 0;
 }
-.postBtn {
-  margin-left: auto;
-  margin-top: 60vh;
-  text-align: center;
-  width: 175px;
-  height: 75px;
-  border-radius: 30px;
-  background-color: #cfd8dc;
-  box-shadow: 1px 3px 2px 1px rgba(0, 0, 0, 0.3);
-}
-.postBtnTxt {
-  font-size: 18px;
-  line-height: 75px;
-}
-.ruleBtn {
-  margin-left: auto;
-  margin-top: 15vh;
-  text-align: center;
-  width: 105px;
-  height: 20px;
-  border-radius: 30px;
-  background-color: #cfd8dc;
-  box-shadow: 1px 3px 2px 1px rgba(0, 0, 0, 0.3);
-}
-.ruleBtnTxt {
-  font-size: 12px;
-  line-height: 20px;
-}
 </style>
 
 <script>
+import PostForm from "@/components/util/PostForm.vue";
+
 export default {
   name: "RightMenu",
   computed: {
@@ -58,6 +26,9 @@ export default {
     logged() {
       return this.$store.getters.logged;
     },
+  },
+  components: {
+    PostForm,
   },
   methods: {
     clickPost: function(){
