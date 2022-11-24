83ea9bc14b476ced6b57d6137695df8ef2414d11
Yudai-Saito <yudai.saito.dev@gmail.com>
2022-11-24 04:11:57 +0000

    refactor: アラートの表示文字と色を文字で指定できるように

--- frontend/src/store/modules/displayAlert.js
+++ frontend/src/store/modules/displayAlert.js
@@ -1,36 +1,43 @@
 const state = {
-  alertState: 0,
+  alertState: "",
   displayAlert: false,
-}
+};
 
 const getters = {
-  alertState: state => state.alertState,
-  displayAlert: state => state.displayAlert,
-}
+  alertState: (state) => state.alertState,
+  displayAlert: (state) => state.displayAlert,
+};
 
 const actions = {
-  alertPost(context) {
-    context.commit('updateAlertState', 0);
-    context.commit('updateAlert', true);
+  alertPostSuccess(context) {
+    context.commit("updateAlertState", "primary");
+    context.commit("updateAlert", true);
     setTimeout(() => {
-      context.commit('updateAlert', false);
+      context.commit("updateAlert", false);
+    }, 3000);
+  },
+  alertPostError(context) {
+    context.commit("updateAlertState", "redAccent2");
+    context.commit("updateAlert", true);
+    setTimeout(() => {
+      context.commit("updateAlert", false);
     }, 3000);
   },
   alertLogin(context) {
-    context.commit('updateAlertState', 1);
-    context.commit('updateAlert', true);
+    context.commit("updateAlertState", "greenLighten2");
+    context.commit("updateAlert", true);
     setTimeout(() => {
-      context.commit('updateAlert', false);
+      context.commit("updateAlert", false);
     }, 3000);
   },
   alertLogout(context) {
-    context.commit('updateAlertState', 2);
-    context.commit('updateAlert', true);
+    context.commit("updateAlertState", "redAccent2");
+    context.commit("updateAlert", true);
     setTimeout(() => {
-      context.commit('updateAlert', false);
+      context.commit("updateAlert", false);
     }, 3000);
   },
-}
+};
 
 const mutations = {
   updateAlertState(state, num) {
@@ -39,11 +46,11 @@ const mutations = {
   updateAlert(state, bool) {
     state.displayAlert = bool;
   },
-}
+};
 
 export default {
   state,
   getters,
   actions,
-  mutations
-}
+  mutations,
+};
