199527a2a627fa4b22aa87386c8de40aefc5d686
Yudai-Saito <yudai.saito.dev@gmail.com>
2022-11-24 06:39:37 +0000

    feat: pickerの絵文字とグループ

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <EmojiPicker />
   </div>
 </template>
+
+<script>
+import EmojiPicker from "@/components/mainContents/EmojiPicker.vue";
+
+export default {
+  components: {
+    EmojiPicker
+  }
+}
+</script>
