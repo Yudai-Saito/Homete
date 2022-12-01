22148d969651bcc70f063cfb3bfa81249fd618b1
Yudai-Saito <yudai.saito.dev@gmail.com>
2022-12-01 01:33:09 +0000

    refactor: リアクション管理中間テーブル

--- backend/models/models.py
+++ backend/models/models.py
@@ -7,7 +7,8 @@ from app import db
 class User(db.Model):
     __tablename__ = "users"
 
-    email = db.Column(db.String(256), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(256), unique=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
     deleted_at = db.Column(db.DateTime)
 
@@ -15,7 +16,7 @@ class Posts(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_email = db.Column(db.String(256), ForeignKey("users.email", name="fk_posts_user_email"))
+    user_id = db.Column(db.Integer, ForeignKey("users.id", name="fk_posts_user_id"))
     private = db.Column(db.Boolean, default=True)
     contents = db.Column(db.String(400))
     name = db.Column(db.String(15))
@@ -32,20 +33,12 @@ class Posts(db.Model):
 class PostReactions(db.Model):
     __tablename__ = "post_reactions"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    post_id = db.Column(db.Integer, ForeignKey("posts.id", name='fk_post_reaction_posts_id'))
-    reaction = db.Column(db.String(10), ForeignKey("reactions.reaction", name='fk_post_reasction_reaction'))
-    reaction_count = db.Column(db.Integer)
-
-class UserReactions(db.Model):
-    __tablename__ = "user_reactions"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_email = db.Column(db.String(256), ForeignKey("users.email", name="fk_user_reaction_posts_user_email"))
-    reaction = db.Column(db.String(10), ForeignKey("reactions.reaction", name='fk_user_reasction_reaction'))
-    post_id = db.Column(db.Integer, ForeignKey("posts.id", name='fk_user_reasction_posts_id'))
+    user_id = db.Column(db.Integer, ForeignKey("users.id", name="fk_post_reactions_user_id"), primary_key=True)
+    post_id = db.Column(db.Integer, ForeignKey("posts.id", name='fk_post_reactions_posts_id'), primary_key=True)
+    reaction_id = db.Column(db.Integer, ForeignKey("reactions.id", name='fk_post_reactions_reaction_id'), primary_key=True)
 
 class Reactions(db.Model):
     __tablename__ = "reactions"
 
-    reaction = db.Column(db.String(10), primary_key=True,)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    reaction = db.Column(db.String(10), unique=True)
