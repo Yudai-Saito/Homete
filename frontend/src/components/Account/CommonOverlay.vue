34c174e596bf38dbb349699d8bcb0fa371525ce6
RenShiratori <ren67nm8@outlook.com>
2022-12-03 17:43:52 +0900

    add:ファイル名の変更と状態に応じて要素の切り替え
    login状態の時は今まで通り
    deletePostかreportPost状態の時はボタンの色を赤にし、下の文章を戻るボタンに切り替え

--- frontend/src/components/Account/CommonOverlay.vue
+++ frontend/src/components/Account/CommonOverlay.vue
@@ -1,13 +1,19 @@
 <template>
   <v-card
     width="400px"
-    height="250px"
-    class="mx-auto mt-5 pa-2 rounded-xl"
+    class="mx-auto pa-2 rounded-xl"
     v-cloak
     v-click-outside="closeOverlayCard"
   >
-    <div>
-      <v-btn icon plain text id="closeOverlayBtn" @click="closeOverlayCard">
+    <div style="margin: 10px">
+      <v-btn
+        v-if="usage == 'login'"
+        icon
+        plain
+        text
+        id="closeOverlayBtn"
+        @click="closeOverlayCard"
+      >
         <v-icon color="#23282F">mdi-close</v-icon>
       </v-btn>
       <v-card-title class="justify-center">{{ titleTxt }}</v-card-title>
@@ -19,7 +25,7 @@
           large
           @click="onClick"
           class="text-transform ma-0 pa-0 rounded-lg"
-          color="white"
+          :color="usage == 'login' ? 'white' : 'red'"
         >
           <img
             v-if="usage == 'login'"
@@ -27,13 +33,29 @@
             src="/assets/google-login-btn.svg"
             @click="onClick"
           />
-          <div id="overlayBtnTxt">{{ btnTxt }}</div>
+          <div
+            :style="usage == 'login' ? 'color: #494854' : 'color: white'"
+            id="overlayBtnTxt"
+          >
+            {{ btnTxt }}
+          </div>
         </v-btn>
       </v-card-actions>
 
-      <v-card-text>
-        <div v-if="usage == 'login'">{{ descriptionTxt }}</div>
+      <v-card-text v-if="usage == 'login'">
+        <div>{{ descriptionTxt }}</div>
       </v-card-text>
+      <v-card-actions v-else class="justify-center">
+        <v-btn
+          style="margin-top: 10px"
+          large
+          @click="closeOverlayCard"
+          class="text-transform ma-0 pa-0 rounded-lg"
+          color="white"
+        >
+          <div id="overlayBtnTxt">戻る</div>
+        </v-btn>
+      </v-card-actions>
     </div>
   </v-card>
 </template>
@@ -45,7 +67,6 @@
   left: 350px !important;
 }
 #overlayBtnTxt {
-  color: #494854;
   margin-right: 8px;
   margin-left: 8px;
   text-transform: none;
@@ -68,7 +89,7 @@ export default {
   ],
   methods: {
     closeOverlayCard: function () {
-      this.$store.dispatch("invisibleLogin");
+      this.$store.dispatch("invisibleCommonOverlay");
     },
   },
 };
