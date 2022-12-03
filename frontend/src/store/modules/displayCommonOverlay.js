25bbaa191607e49660e0e3754f416b28dc8d01e5
RenShiratori <ren67nm8@outlook.com>
2022-12-03 17:42:02 +0900

    refact:ログインオーバーレイ状態管理を汎用的に使うようにしたためファイル名を変更

---
+++ frontend/src/store/modules/displayCommonOverlay.js
@@ -0,0 +1,36 @@
+const state = {
+  displayCommonOverlay: false,
+  overlayState: "",
+};
+
+const getters = {
+  overlayState: (state) => state.overlayState,
+};
+
+const actions = {
+  invisibleCommonOverlay(context) {
+    context.commit("updateOverlayState", "");
+  },
+  visibleLoginOverlay(context) {
+    context.commit("updateOverlayState", "login");
+  },
+  visibleDeletePostOverlay(context) {
+    context.commit("updateOverlayState", "deletePost");
+  },
+  visibleReportPostOverlay(context) {
+    context.commit("updateOverlayState", "reportPost");
+  },
+};
+
+const mutations = {
+  updateOverlayState(state, str) {
+    state.overlayState = str;
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
