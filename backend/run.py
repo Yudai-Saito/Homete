37848becd376f0fe19b3c75e4e018eacca52f1ae
YudaiSaito <yudai.saito.dev@gmail.com>
2022-12-10 23:21:05 +0900

    feat: socketIOエンドポイントの起動とルーティング

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,7 @@
 from app import app
+from socketIO.socket_endpoint import socketio
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
+
+
