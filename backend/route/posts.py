feee91adf0b9edc06a488378e63b5b42d517d94a
YudaiSaito <yudai.saito.dev@gmail.com>
2022-12-13 22:21:37 +0900

    refactor: 投稿取得用クエリの切り出し
    socketio側とAPI側で使用するため切り分け

--- backend/route/posts.py
+++ backend/route/posts.py
@@ -15,6 +15,7 @@ from models.models import User, Posts, PostReactions, Reactions
 from util.auth_decorator import auth_required
 from util.jwt_decoder import get_email_from_cookie
 from util.icon_generator import open_peeps_icon
+from util.get_posts import get_posts
 
 posts = Blueprint("posts", __name__, url_prefix="/posts")
 
@@ -74,46 +75,11 @@ def post_get():
 			user_email = get_email_from_cookie(jwt)
 			user_id = db.session.query(User.id).filter(User.email == user_email).first()[0]
 
-		created_at = request.args.get("created_at")
 		update = request.args.get("update")
+		created_at = request.args.get("created_at")
 		channel = request.args.get("channel")
 
-		filters = []
-		if update == "new":
-			filters.append(created_at < Posts.created_at)
-		elif update == "old":
-			filters.append(created_at > Posts.created_at)
-		else:
-			filters.append("2000-01-01" < Posts.created_at)
-
-		if channel == "history":
-			filters.append(Posts.user_id == user_id)
-
-		reaction_subquery = db.session.query(Reactions.reaction).filter(Reactions.id == PostReactions.reaction_id).label("post_reactions")
-  
-		post_reaction_subquery = db.session.query(PostReactions.post_id, reaction_subquery, func.count(PostReactions.reaction_id).label("reaction_count"))\
-															.group_by(PostReactions.post_id, PostReactions.reaction_id).subquery()
-
-		reaction_subquery = db.session.query(Reactions.reaction).filter(Reactions.id == PostReactions.reaction_id, PostReactions.user_id == user_id)
-
-		user_reaction_subquery = db.session.query(PostReactions.post_id, func.json_arrayagg(reaction_subquery, type_=JSON)\
-															.label("user_reactions")).filter(PostReactions.user_id == user_id).group_by(PostReactions.post_id).subquery()
-
-		posts = db.session.query(func.json_object("post_id", Posts.id, \
-							"icon", func.json_object("head", Posts.head, "face", Posts.face, "facial_hair", Posts.facialhair, "accessories", Posts.accessories,\
-																					"skin_color", Posts.skincolor, "clothing_color", Posts.clothingcolor, "hair_color", Posts.haircolor, type_=JSON),\
-								"name", Posts.name, "created_at", Posts.created_at, "contents", Posts.contents, 
-									"private", case([(Posts.user_id == user_id, False)], else_=Posts.private),\
-										"user_post", case([(Posts.user_id == user_id, True)], else_=False),\
-											"post_reactions", func.json_arrayagg(func.json_object("reaction", post_reaction_subquery.c.post_reactions,\
-												"count", case([(Posts.user_id == user_id, post_reaction_subquery.c.reaction_count),\
-													(Posts.private == True, None)], else_=post_reaction_subquery.c.reaction_count)), type_=JSON),\
-														"user_reaction", user_reaction_subquery.c.user_reactions, type_=JSON))\
-															.outerjoin(post_reaction_subquery, post_reaction_subquery.c.post_id == Posts.id)\
-															.outerjoin(user_reaction_subquery, user_reaction_subquery.c.post_id == post_reaction_subquery.c.post_id)\
-																.filter(and_(*filters, Posts.deleted_at == None))\
-																	.group_by(Posts.id, post_reaction_subquery.c.post_id, user_reaction_subquery.c.post_id, user_reaction_subquery.c.user_reactions)\
-																		.order_by(desc(Posts.created_at)).limit(30).all()
+		posts = get_posts(user_id, update, created_at, channel)
   
 		return jsonify({"posts": [row[0] for row in posts]}), 200
 	except:
