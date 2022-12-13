6eb933098e38c68fd2478b537c7235393dcd85bc
YudaiSaito <yudai.saito.dev@gmail.com>
2022-12-13 22:23:17 +0900

    feat: 更新投稿をwebsocketでコネクションユーザに送信

--- backend/socketIO/socket_endpoint.py
+++ backend/socketIO/socket_endpoint.py
@@ -1,11 +1,14 @@
-from app import app
+from app import app, session
 
 from os import environ
+from json import dumps
 
 from flask_socketio import SocketIO
-from flask_socketio import emit, send
+from flask_socketio import emit
 
-socketio = SocketIO(app, cors_allowed_origins=[environ["FRONTEND_URL"]])
+from util.get_posts import get_posts
+
+socketio = SocketIO(app, cors_allowed_origins=[environ["FRONTEND_URL"]], cors_credentials=True )
 
 @socketio.on('connect')
 def connect():
@@ -15,7 +18,8 @@ def connect():
 def on_disconnect():
     print('Client disconnected')
 
-@socketio.on('hello')
-def handle_message(data):
-    print('received message: ' + data)
-    emit("hello", "world")
+def emit_posts(update_time_start, update_time_end):
+    with session.begin():
+        posts = get_posts(user_id=None, update="new", created_at="", channel="", update_time_start=update_time_start, update_time_end=update_time_end)
+
+    socketio.emit("new_posts", dumps({"posts": [row[0] for row in posts]}, ensure_ascii=False))
