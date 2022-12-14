46e26166b713700c1cf71d03b7592f33fc62a38f
Yudai-Saito <yudai.saito.dev@gmail.com>
2022-12-14 00:14:41 +0000

    feat: 投稿完了時に投稿完了情報をレスポンスする

---
+++ backend/util/posts_template.py
@@ -0,0 +1,16 @@
+user_posts_template = {
+      "contents": "", 
+      "created_at": "", 
+      "icon": {}, 
+      "name": "", 
+      "post_id": None, 
+      "post_reactions": [
+        {
+          "count": None, 
+          "reaction": None
+        }
+      ], 
+      "private": 0, 
+      "user_post": 1, 
+      "user_reaction": None
+}
