3e1e89c64f7a4b5ed50f98b8e87e0efc98b5831b
Yudai-Saito <yudai.saito.dev@gmail.com>
2023-01-11 00:42:12 +0000

    feat: 投稿削除画面から削除ボタン押して削除できるように

--- admin/routes/posts.py
+++ admin/routes/posts.py
@@ -1,8 +1,10 @@
+import datetime
+
 from app import db
-from models.models import Posts
+from models.models import Posts, PostReactions
 
 from sqlalchemy import desc
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
 posts = Blueprint("posts", __name__, url_prefix="/posts")
 
@@ -29,3 +31,17 @@ def posts_delete_template():
   posts = db.session.query(Posts).filter(Posts.id == post_id).first()
 
   return render_template("postsDelete.html", posts=posts)
+
+@posts.route("/destroy")
+def posts_destoroy():
+  post_id = request.args.get("post_id")
+
+  db.session.query(PostReactions).filter(PostReactions.post_id == post_id).delete()
+
+  post = db.session.query(Posts).filter(Posts.id == post_id).first()
+  post.approved = False
+  post.deleted_at = datetime.datetime.now()
+  
+  db.session.commit()
+
+  return redirect(url_for("posts.posts_template"))
