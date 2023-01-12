a4e2c86c567ee6d7ced27d3c71427285ffe4569b
Yudai-Saito <yudai.saito.dev@gmail.com>
2023-01-12 02:25:42 +0000

    feat: 管理者用の機能はすべて認証を必須とする

--- admin/routes/posts.py
+++ admin/routes/posts.py
@@ -6,14 +6,18 @@ from models.models import Posts, PostReactions, ReportPosts
 from sqlalchemy import desc, func
 from flask import Blueprint, render_template, request, redirect, url_for, session
 
+from util.auth_decorator import auth_required
+
 posts = Blueprint("posts", __name__, url_prefix="/posts")
 
 @posts.route("/")
+@auth_required
 def posts_template():
   posts = db.session.query(Posts).filter(Posts.approved == None).order_by(desc(Posts.id)).all()
   return render_template("posts.html", display_name="全体投稿" ,posts=posts)
 
 @posts.route("/report")
+@auth_required
 def posts_report_template():
     report_posts = db.session.query(ReportPosts.post_id).all()
     report_posts = [post.post_id for post in report_posts] 
@@ -23,6 +27,7 @@ def posts_report_template():
     return render_template("posts.html", display_name="通報投稿", posts=posts)
 
 @posts.route("/approved")
+@auth_required
 def posts_approved():
   post_id = request.args.get("post_id")
   
@@ -36,6 +41,7 @@ def posts_approved():
   return redirect(request.referrer)
 
 @posts.route("/delete")
+@auth_required
 def posts_delete_template():
   post_id = request.args.get("post_id")
 
@@ -47,6 +53,7 @@ def posts_delete_template():
   return render_template("postsDelete.html", posts=posts, delete_count=delete_count[0][0], referrer=request.referrer)
 
 @posts.route("/destroy")
+@auth_required
 def posts_destoroy():
   post_id = request.args.get("post_id")
 
