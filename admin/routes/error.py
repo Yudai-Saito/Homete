73bc5bcace27c425a978f7ff1b124a35519a310c
Yudai-Saito <yudai.saito.dev@gmail.com>
2023-01-12 02:26:06 +0000

    fix: エラーハンドリングを例外発生ごとに表示変更

--- admin/routes/error.py
+++ admin/routes/error.py
@@ -5,9 +5,10 @@ from flask import Blueprint, render_template, redirect, url_for
 error = Blueprint("error", __name__, url_prefix="/error")
 
 @error.route("/")
-def error_template():
-  return render_template('error.html'), 500
+def error_template(error=""):
+  return render_template('error.html', error=error), 500
 
-@error.errorhandler(Exception)
-def handle_error(e):
-  return redirect(url_for('error'))
+@app.errorhandler(Exception)
+def handle_error(error):
+  return error_template(error)
+
