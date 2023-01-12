a5f56e3808fad8e952ba2b45e2c8e72a8b633132
Yudai-Saito <yudai.saito.dev@gmail.com>
2023-01-12 03:02:53 +0000

    chore: 管理者用firebaseアカウントの秘密鍵読み取り

--- admin/app.py
+++ admin/app.py
@@ -3,7 +3,7 @@ from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-#from firebase_admin import credentials, initialize_app
+from firebase_admin import credentials, initialize_app
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -17,8 +17,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 
-#cred = credentials.Certificate("firebase_account_key.json")
-#firebase_app = initialize_app(cred)
+cred = credentials.Certificate("firebase_account_key.json")
+firebase_app = initialize_app(cred)
 
 from routes.error import error
 from routes.login import login
