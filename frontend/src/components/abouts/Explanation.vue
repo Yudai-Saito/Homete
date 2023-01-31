4d85351da08c20dab18e610d3d409b50b39e816a
YudaiSaito <yudai.saito.dev@gmail.com>
2023-01-31 15:17:39 +0900

    style: タイトルとラインにマージン追加

--- frontend/src/components/abouts/Explanation.vue
+++ frontend/src/components/abouts/Explanation.vue
@@ -4,10 +4,12 @@
       <div v-twemoji style="width: 20px; margin-right: 5px; margin-top: 12px">
         🔰
       </div>
-      <h3 v-twemoji class="aboutTitleTxt">HOMETEの使い方</h3>
+      <h3 v-twemoji class="aboutTitleTxt" style="margin: 10px">
+        HOMETEの使い方
+      </h3>
     </div>
-    <v-divider />
-    <p class="aboutText">使い方の説明</p>
+    <v-divider style="margin: 10px" />
+    <p class="aboutText">準備中...</p>
   </div>
 </template>
 
