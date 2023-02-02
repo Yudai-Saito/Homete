7a632c64e3ef253bc0d97028a1692a737cf3882a
Yudai-Saito <yudai.saito.dev@gmail.com>
2023-02-02 07:04:48 +0000

    fix: リアクション数のバリデーターで15個以上つかないバグを修正

--- backend/validator/post_reaction_validator.py
+++ backend/validator/post_reaction_validator.py
@@ -1,10 +1,20 @@
 from sqlalchemy import func
 
 from app import db
-from models.models import PostReactions
+from models.models import PostReactions, Reactions
 
-def posts_reaction_count_validate(post_id):
-    posts_reaction_count = db.session.query(func.count(PostReactions.post_id)).filter(PostReactions.post_id == post_id).all()
-    print(posts_reaction_count)
-    if posts_reaction_count[0][0] >= 15:
+DEFAULT_REACTION = ["👍", "👀", "💯", "🥰", "🎉"]
+
+def posts_reaction_count_validate(post_id, reaction_id):
+    reaction = db.session.query(Reactions.reaction).filter(Reactions.id == reaction_id).first()[0]
+    exists_reaction = db.session.query(PostReactions.query.filter(PostReactions.reaction_id == reaction_id).exists()).scalar()
+
+    if reaction in DEFAULT_REACTION or exists_reaction:
+        return
+
+    posts_reaction_count = db.session.query(func.count(func.distinct(Reactions.id))) \
+                .join(PostReactions, Reactions.id == PostReactions.reaction_id) \
+                    .filter(PostReactions.post_id == post_id, ~Reactions.reaction.in_(DEFAULT_REACTION)).scalar()
+
+    if posts_reaction_count >= 15:
         raise ValueError("over reaction")
