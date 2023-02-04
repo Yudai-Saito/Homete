20d3b8bbd7a19631c5c5e37800bd66aee9615ef8
YudaiSaito <yudai.saito.dev@gmail.com>
2023-02-04 13:38:59 +0900

    chore: [admin] firebaseアプリのAPIKEYの切り分け

---
+++ admin/static/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCGPZHbYUfjE4xlLvJJbHTwxradkechwiI",
+  authDomain: "homete-admin.firebaseapp.com",
+  projectId: "homete-admin",
+  storageBucket: "homete-admin.appspot.com",
+  messagingSenderId: "614569679417",
+  appId: "1:614569679417:web:707a5abb1fca0243d44be8",
+  measurementId: "G-4LV39DSNFJ",
+};
