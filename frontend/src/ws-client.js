675e428adff3a45a6cb0ab200370c633ce2fb8a5
YudaiSaito <yudai.saito.dev@gmail.com>
2023-02-04 18:50:22 +0900

    refactor: WSをクラス化してWS切断メソッドを追加

--- frontend/src/ws-client.js
+++ frontend/src/ws-client.js
@@ -1,47 +1,50 @@
 import { io } from "socket.io-client";
 
-function ws_connect(vm) {
-  const socket = io(process.env.VUE_APP_API_ENDPOINT, {
-    withCredentials: true,
-    transports: ["websocket"],
-    cookie: true,
-  });
+class WSService {
+  socket = null;
 
-  socket.on("connect", () => {
-    console.log("connect");
-    socket.emit("hello", "world");
-  });
+  connect(vm) {
+    this.socket = io(process.env.VUE_APP_API_ENDPOINT, {
+      withCredentials: true,
+      transports: ["websocket"],
+      cookie: true,
+    });
 
-  socket.io.on("error", (error) => {
-    console.log(error);
-  });
+    this.socket.on("new_posts", (data) => {
+      let dataJson = JSON.parse(data);
+      let posts = dataJson["posts"];
 
-  socket.on("new_posts", (data) => {
-    let dataJson = JSON.parse(data);
-    let posts = dataJson["posts"];
+      let existsPosts = JSON.parse(
+        JSON.stringify(vm.$store.getters.completedPost)
+      );
 
-    let existsPosts = JSON.parse(
-      JSON.stringify(vm.$store.getters.completedPost)
-    );
-
-    //投稿したモノが入っていた場合は削除して追加
-    for (let i = 0; i < existsPosts.length; i++) {
-      for (let j = 0; j < posts.length; j++) {
-        if (posts[j].post_id == existsPosts[i].post_id) {
-          posts.splice(j, 1);
-          vm.$store.commit("deleteIntexCompletedPost", existsPosts[i]);
+      //投稿したモノが入っていた場合は削除して追加
+      for (let i = 0; i < existsPosts.length; i++) {
+        for (let j = 0; j < posts.length; j++) {
+          if (posts[j].post_id == existsPosts[i].post_id) {
+            posts.splice(j, 1);
+            vm.$store.commit("deleteIntexCompletedPost", existsPosts[i]);
+          }
         }
       }
-    }
 
-    if (posts.length > 0) {
-      vm.$store.commit("setUpdatePosts", posts);
+      if (posts.length > 0) {
+        vm.$store.commit("setUpdatePosts", posts);
 
-      if (vm.$store.getters.contentsKey == "timeline") {
-        vm.$store.dispatch("alertNewPost");
+        if (vm.$store.getters.contentsKey == "timeline") {
+          vm.$store.dispatch("alertNewPost");
+        }
       }
+    });
+  }
+
+  disconnect() {
+    if (this.socket) {
+      this.socket.close();
+      this.socket = null;
     }
-  });
+  }
 }
 
-export default { ws_connect };
+const ws = new WSService();
+export default ws;
