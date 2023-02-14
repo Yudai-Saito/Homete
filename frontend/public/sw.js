38cb7c94f2523230bccd86062ef3fd2998498a43
RenShiratori <ren67nm8@outlook.com>
2023-02-14 13:10:18 +0900

    add:アラートダイアログからページのリロードをかけられるようにした
    そもそも起動時にswが更新されているようなのでいらないコミットの可能性あり

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,2 +1,14 @@
 self.addEventListener("fetch", () => {});
-var cacheName = "1.0.0";
+
+//メッセージを受け取ったとき
+self.addEventListener("message", function (event) {
+  switch (event.data) {
+    case "updateCache":
+      event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+          return cache.addAll(urlsToCache);
+        })
+      );
+      break;
+  }
+});
